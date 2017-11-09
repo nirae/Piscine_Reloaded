c62db474d10fae74a7a4175d8f79fde3dfa28c8b
ndubouil <ndubouil@student.42.fr>
2017-11-09 12:04:29 +0100

    ADD ex14, ex15, ex16, ex17, ex18 | OK

---
+++ ex18/ft_print_params.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ndubouil <ndubouil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/09 11:42:36 by ndubouil          #+#    #+#             */
+/*   Updated: 2017/11/09 12:02:40 by ndubouil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	int i;
+
+	i = 1;
+	if (argc > 0)
+	{
+		while (i < argc)
+		{
+			ft_putstr(argv[i]);
+			ft_putchar('\n');
+			i++;
+		}
+	}
+	return (0);
+}
